b310ca23cbc0b1d8f2b5c116de690660fe73f53d
chep <51736642+MaryanneChepkoech@users.noreply.github.com>
2021-06-05 00:42:25 +0300

    Problem 1

--- problem1.py
+++ problem1.py
@@ -3,15 +3,14 @@ import sys
 
 def main():
     print("What is your name?")
-    name=input()
+    name = input()
     print("Name:" + name)
     print("How old are you?")
-    age =input()
+    age = input()
     print('Age:' + age)
     x = "Maryanne"
     y = "28 years old"
     print(f"Her name is {x} and she is {y}")
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     sys.exit(main())
