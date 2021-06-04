b1d165f8c521849c9e2dd58840e2c9692237c641
chep <51736642+MaryanneChepkoech@users.noreply.github.com>
2021-06-05 00:36:10 +0300

    Problem 1

---
+++ problem1.py
@@ -0,0 +1,17 @@
+import sys
+
+
+def main():
+    print("What is your name?")
+    name=input()
+    print("Name:" + name)
+    print("How old are you?")
+    age =input()
+    print('Age:' + age)
+    x = "Maryanne"
+    y = "28 years old"
+    print(f"Her name is {x} and she is {y}")
+
+
+if __name__=="__main__":
+    sys.exit(main())
