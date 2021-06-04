ae2644908a1f932132d84f02ae5be91a92a3d709
chep <51736642+MaryanneChepkoech@users.noreply.github.com>
2021-06-05 01:20:23 +0300

    problem2.py

--- problem2.py
+++ problem2.py
@@ -1,10 +1,27 @@
 import sys
 
 
-def main()
-
-    Print('Name:')
+def main():
+    print("What is your name?")
     name = input()
-    print("Her name is"+ name)
-if __name__=='main':
+    print("Name:" + name)
+    print("How old are you?")
+    age = input()
+    print('Age:' + age)
+    print("What year were you born?")
+    birthdt= input()
+    print("year:" + birthdt)
+    w=birthdt
+    #print(w)
+    v = (int(w) + 77)
+    #print(v)
+    x = name
+    y = age
+    z = birthdt
+
+    print(f"Her name is {x}", f"she is {y}" ,f"Born in {z}",f"She will be 77years old in {v}" ,sep=";")
+
+
+
+if __name__=='__main__':
     sys.exit(main())
