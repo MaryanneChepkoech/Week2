b52ee67cee0faa4655d20abdf6de487bb537a7e2
chep <51736642+MaryanneChepkoech@users.noreply.github.com>
2021-06-21 23:30:54 +0300

    Problem5.py

---
+++ Problem5.py
@@ -0,0 +1,21 @@
+import math
+import sys
+import math
+
+
+def calculate(a, b, c)
+    # control+alt+l to clean code
+    x = -b + math.sqrt(b ** 2 - 4 * a * c)
+    return x
+
+
+def main()
+    c = 2
+    b = 3
+    a = 4
+    x = calculate(a, b, c)
+    print(f"x={x}")
+
+
+if __name__ == "__main__"
+    sys.exit(main())
