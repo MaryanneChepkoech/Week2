d5500fa3188561b5196f3ddfdff60bb154fa40cb
chep <51736642+MaryanneChepkoech@users.noreply.github.com>
2021-06-21 23:39:49 +0300

    Problem5.py

--- Problem5.py
+++ Problem5.py
@@ -2,19 +2,19 @@ import sys
 import math
 
 
-def calculate(a, b, c)
+def calculate(a, b, c):
     # control+alt+l to clean code
-    x = -b + math.sqrt(b ** 2 - 4 * a * c)
+    x = 2 * c/(-b + math.sqrt(b ** 2 - 4 * a * c))
     return x
 
 
-def main()
-    c = 2
-    b = 3
-    a = 4
+def main():
+    c = 3
+    b = 2
+    a = 1
     x = calculate(a, b, c)
     print(f"x={x}")
 
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     sys.exit(main())
